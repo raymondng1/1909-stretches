50357cead56f2fba4b3b8578ade0d879832bd0af
Raymond Ng <raymond.ng47@gmail.com>
2019-10-21 19:05:51 -0400

    trim solution

--- questions/09_trim/trim.js
+++ questions/09_trim/trim.js
@@ -3,7 +3,7 @@ const trim = (obj) => {
 
     for(const key in obj){
         if(obj[key] === null || obj[key] === undefined){
-            delete obj[key]
+            delete(obj[key])
         }
     }
     return newObj;
